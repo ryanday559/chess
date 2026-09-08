be636f79f3ea4dc1f28da529153667627e3bd512
ryanday559 <ryanday559@gmail.com>
2026-09-08 16:52:28 -0600

    working on separating individual functions into cardinal directions

--- shared/src/main/java/chess/Bishop.java
+++ shared/src/main/java/chess/Bishop.java
@@ -1,20 +1,29 @@
 package chess;
 
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
 import java.util.Collection;
 import java.util.HashSet;
 
 public class Bishop extends ChessPiece {
     private HashSet<ChessMove> pieceSpecificMoves(HashSet<ChessMove> moveOptions, ChessBoard board, ChessPosition myPosition) {
-        int startingRow = myPosition.getRow();
-        int startingColumn = myPosition.getColumn();
-        ChessPosition northWestPosition = new ChessPosition(startingRow + 1, startingColumn - 1);
-        while (isValidBishopMove(board, northWestPosition)) {
-            moveOptions.add(northWestPosition);
-        }
+        moveOptions = checkNorthwestMoves(moveOptions, board, myPosition);
+
         return moveOptions;
     }
 
-    private boolean PieceMoveType(ChessBoard board, ChessPosition position) {
+    private HashSet<ChessMove> checkNorthwestMoves(HashSet<ChessMove> moveOptions, ChessBoard board, ChessPosition myPosition) {
+        int startingRow = myPosition.getRow();
+        int startingColumn = myPosition.getColumn();
+        ChessPosition northWestPosition = new ChessPosition(startingRow + 1, startingColumn - 1);
+        while (checkPieceMoveType(board, northWestPosition) != PieceMoveType.INVALID) {
+            moveOptions.add(northWestPosition);
+            startingRow += 1;
+            startingColumn -= 1;
+            northWestPosition = new ChessPosition(startingRow + 1, startingColumn - 1);
+        }
+    }
+
+    private boolean checkPieceMoveType(ChessBoard board, ChessPosition position) {
         ChessPiece spaceOccupant = board.getPiece(position);
         if (!position.isValidPosition() || spaceOccupant.getTeamColor() == this.getTeamColor()) return PieceMoveType.INVALID;
         else if (spaceOccupant == null) return PieceMoveType.EMPTY;
