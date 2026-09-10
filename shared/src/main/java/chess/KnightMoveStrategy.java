3c4e27bc7c19b2fdb28255c6a70b71a1cd6b2fa6
ryanday559 <ryanday559@gmail.com>
2026-09-10 10:23:31 -0600

    added KnightMoveStrategy

---
+++ shared/src/main/java/chess/KnightMoveStrategy.java
@@ -0,0 +1,19 @@
+package chess;
+
+class KnightMoveStrategy implements MoveStrategy {
+    // We need to check up two over one and up one over two in all NeNwSeSw directions.
+    @Override
+    int[][] singleMoveOffsets = {
+        // Ne
+        {2,1}, {1,2},
+
+        // Nw
+        {2,-1}, {1,-2},
+
+        // Se
+        {-2,1}, {-1,2},
+
+        // Sw
+        {-2,-1}, {-1,-2}
+    };
+}
