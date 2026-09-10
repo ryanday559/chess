0f14163a091b7dc84bf012309eecb55be48ecf66
ryanday559 <ryanday559@gmail.com>
2026-09-10 09:55:21 -0600

    updated BishopMoveStrategy to use offsets rather than individual funcs

--- shared/src/main/java/chess/BishopMoveStrategy.java
+++ shared/src/main/java/chess/BishopMoveStrategy.java
@@ -5,70 +5,26 @@ import java.util.List;
 
 class BishopMoveStrategy implements MoveStrategy {
     private List<ChessPosition> validMoves = new ArrayList<>();
+    private int[][] offsets = {{1,-1}, {1,1}, {-1,1}, {-1,-1}};
+
 
     @Override
     public List<ChessPosition> getValidMoves(ChessPosition position, ChessBoard board){
         // We need to check NW, NE, SE, and SW
-        checkNwMoves(position, board);
-        checkNeMoves(position, board);
-        checkSwMoves(position, board);
-        checkSeMoves(position, board);
+        int startingRow = position.getRow();
+        int startingColumn = position.getColumn();
+        for (int[] offset : offsets){
+            int nextRow = startingColumn + offset[0];
+            int nextColumn = startingRow + offset[1];
+            ChessPosition nextPosition = new ChessPosition(nextRow, nextColumn);
+            while (board.canMove(position, nextPosition)) {
+                validMoves.add(nextPosition);
+                nextRow += offset[0];
+                nextColumn += offset[1];
+                nextPosition = new ChessPosition(nextRow, nextColumn);
+            }
+        }
         return validMoves;
     }
 
-    private void checkNwMoves(ChessPosition startPosition, ChessBoard board) {
-        int startRow = startPosition.getRow();
-        int startColumn = startPosition.getColumn();
-        int nextRow = startRow + 1;
-        int nextColumn = startColumn - 1;
-        ChessPosition nextPosition = new ChessPosition(nextRow, nextColumn);
-        while(board.canMove(startPosition, nextPosition)) {
-            validMoves.add(nextPosition);
-            nextRow += 1;
-            nextColumn -= 1;
-            nextPosition = new ChessPosition(nextRow, nextColumn);
-        }
-    }
-
-    private void checkNeMoves(ChessPosition startPosition, ChessBoard board) {
-        int startRow = startPosition.getRow();
-        int startColumn = startPosition.getColumn();
-        int nextRow = startRow + 1;
-        int nextColumn = startColumn + 1;
-        ChessPosition nextPosition = new ChessPosition(nextRow, nextColumn);
-        while(board.canMove(startPosition, nextPosition)) {
-            validMoves.add(nextPosition);
-            nextRow += 1;
-            nextColumn += 1;
-            nextPosition = new ChessPosition(nextRow, nextColumn);
-        }
-    }
-
-    private void checkSwMoves(ChessPosition startPosition, ChessBoard board) {
-        int startRow = startPosition.getRow();
-        int startColumn = startPosition.getColumn();
-        int nextRow = startRow - 1;
-        int nextColumn = startColumn - 1;
-        ChessPosition nextPosition = new ChessPosition(nextRow, nextColumn);
-        while(board.canMove(startPosition, nextPosition)) {
-            validMoves.add(nextPosition);
-            nextRow -= 1;
-            nextColumn -= 1;
-            nextPosition = new ChessPosition(nextRow, nextColumn);
-        }
-    }
-
-    private void checkSeMoves(ChessPosition startPosition, ChessBoard board) {
-        int startRow = startPosition.getRow();
-        int startColumn = startPosition.getColumn();
-        int nextRow = startRow - 1;
-        int nextColumn = startColumn + 1;
-        ChessPosition nextPosition = new ChessPosition(nextRow, nextColumn);
-        while(board.canMove(startPosition, nextPosition)) {
-            validMoves.add(nextPosition);
-            nextRow -= 1;
-            nextColumn += 1;
-            nextPosition = new ChessPosition(nextRow, nextColumn);
-        }
-    }
 }
