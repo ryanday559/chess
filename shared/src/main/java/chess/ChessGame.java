89e74f8ac8fa8709be357f55be0de90f021d36fc
ryanday559 <ryanday559@gmail.com>
2026-09-22 15:21:59 -0600

    Added a getter and a setter for board in chess game

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -11,6 +11,7 @@ import java.util.Collection;
 public class ChessGame {
 
     private TeamColor currentTurn = TeamColor.WHITE;
+    private ChessBoard board;
 
 
     public ChessGame() {
@@ -99,7 +100,7 @@ public class ChessGame {
      * @param board the new board to use
      */
     public void setBoard(ChessBoard board) {
-        throw new RuntimeException("Not implemented");
+        this.board = board;
     }
 
     /**
@@ -108,6 +109,6 @@ public class ChessGame {
      * @return the chessboard
      */
     public ChessBoard getBoard() {
-        throw new RuntimeException("Not implemented");
+        return this.board;
     }
 }
